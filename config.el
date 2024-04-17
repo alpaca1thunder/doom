15a74e8c313aaa8c1084efcc85ed2b0c727db051
Alex Taylor <ataylor@posteo.us>
2024-04-17 15:49:05 -0400

    corrected japanese font fix

--- config.el
+++ config.el
@@ -7,7 +7,9 @@
    ;; Doom font
    (setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 15))
    ;; Monospace Japanese font
-(set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 24))))))
+   (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 24))))))
+;; Set monospace Japanese font even when not running as a client/daemon
+(set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 24))
 ;; Line Numbers
 (setq display-line-numbers-type t)
 ;; Org Directory
