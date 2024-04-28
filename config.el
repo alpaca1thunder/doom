e47099abdeeef3fe9a09bdd0666ef415e45ffd26
Alex Taylor <ataylor@posteo.us>
2024-04-28 13:56:02 -0400

    Revert "increased font on desktop"
    This reverts commit 002bd9cbb2d105a7f042e416894c00262080874a.

--- config.el
+++ config.el
@@ -5,9 +5,9 @@
    ;; Doom theme
    (setq doom-theme 'doom-nord)
    ;; Doom font
-   (setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 19))
+   (setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 17))
    ;; Monospace Japanese font
-   (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 25))))))
+   (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 23))))))
 ;; Set monospace Japanese font even when not running as a client/daemon
 (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 23))
 ;; Line Numbers
