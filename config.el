e79258c8a4f526ce8ebf52ac7f4318f161e36471
Alex Taylor <ataylor@posteo.us>
2024-06-07 10:40:45 -0400

    switched to gpt-4o

--- config.el
+++ config.el
@@ -82,7 +82,7 @@ Call this on `flyspell-incorrect-hook'."
 ;; gptel
 (setq gptel-api-key (shell-command-to-string "bw get password d0cac8bb-14d4-4b5a-b371-b14500e2f1c5"))
 (setq gptel-default-mode 'org-mode)
-(setq gptel-model "gpt-4-turbo")
+(setq gptel-model "gpt-4o")
 ;; Fix evil search
 ;; https://github.com/doomemacs/doomemacs/issues/6478#issuecomment-1406167570
 (setq org-fold-core-style 'overlays)
